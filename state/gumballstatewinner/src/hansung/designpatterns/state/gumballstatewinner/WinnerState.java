2e56150390acd429da6d76acbee84fc0a880c9b4
Kwanwoo Lee 70 <kwanu.lee@gmail.com>
2021-05-02 21:16:36 +0900

    WinnerState update

--- state/gumballstatewinner/src/hansung/designpatterns/state/gumballstatewinner/WinnerState.java
+++ state/gumballstatewinner/src/hansung/designpatterns/state/gumballstatewinner/WinnerState.java
@@ -20,18 +20,13 @@ public class WinnerState implements State {
 	}
  
 	public void dispense() {
+		System.out.println("YOU'RE A WINNER! You got two gumballs for your quarter");
+		gumballMachine.releaseBall();
 		gumballMachine.releaseBall();
 		if (gumballMachine.getCount() == 0) {
 			gumballMachine.setState(gumballMachine.getSoldOutState());
 		} else {
-			gumballMachine.releaseBall();
-			System.out.println("YOU'RE A WINNER! You got two gumballs for your quarter");
-			if (gumballMachine.getCount() > 0) {
-				gumballMachine.setState(gumballMachine.getNoQuarterState());
-			} else {
-            	System.out.println("Oops, out of gumballs!");
-				gumballMachine.setState(gumballMachine.getSoldOutState());
-			}
+			gumballMachine.setState(gumballMachine.getNoQuarterState());
 		}
 	}
 	
